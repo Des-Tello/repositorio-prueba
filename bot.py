f6236c91608051b0e3fc0a80c8cea48fbf025c06
Rodrigo <rodrigo.ordenes.a@gmail.com>
2021-02-21 02:07:18 -0300

    Edición_1_Rodrigo

--- bot.py
+++ bot.py
@@ -3,21 +3,10 @@ import pywhatkit
 
 try:
     #envio de mensaje
-    pywhatkit.sendwhatmsg("+56920450536","Hola amigito",2,15)
+    pywhatkit.sendwhatmsg("+56920450536","Hola amigito jijijijjiji tello ql chupala xdddddddddddd el kuki es gei",2,15)
     print("enviado exitosamente")
 
 except:
     print("algo salio mal")
 '''
-
-import pyautogui as pg, webbrowser as web, time as tm
-
-web.open("https://web.whatsapp.com/send?phone=+56920450536")
-i=0
-tm.sleep(8)
-while (True):
-    pg.write("Hola amigito")
-    i = i+1
-    if(i>5000):break;
-    pg.press('enter')asdasdasda
 
